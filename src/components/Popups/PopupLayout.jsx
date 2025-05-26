87f76f5e7f0b3221ae6433fb591ab9e0471f75e7
E <eric.sirheimer@gmail.com>
2025-05-26 13:33:40 +0200

    New design of all existing popups / modals

--- src/components/Popups/PopupLayout.jsx
+++ src/components/Popups/PopupLayout.jsx
@@ -1,10 +1,11 @@
 // PopupLayout.js
-import React from 'react';
+import React, { useState } from 'react';
 import Modal from 'react-modal';
-import Spinner from '../Shared/Spinner';
-import Header from '../Layout/Header';
 
-const PopupLayout = ({ isOpen, onClose, loading = false, fullScreen = false, children }) => {
+import Header from '@/components/Layout/Header';
+import Spinner from '@/components/Shared/Spinner';
+
+const PopupLayout = ({ isOpen, isClosing, onClose, loading = false, fullScreen = false, children, className = '' }) => {
 
 	if (!isOpen) return null;
 
@@ -15,21 +16,33 @@ const PopupLayout = ({ isOpen, onClose, loading = false, fullScreen = false, chi
 				className="absolute inset-0 flex items-center justify-center"
 				overlayClassName="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
 			>
-				<Spinner />
+				<Spinner size="small" />
 			</Modal>
 		);
 	}
+
 	return (
 		<Modal
 			isOpen={isOpen}
 			onRequestClose={onClose}
-			className={`bg-gray-50 dark:bg-gray-700 relative overflow-y-auto custom-scrollbar overflow-x-hidden ${fullScreen ? 'flex flex-col space-between w-full h-full' : 'p-4 w-full sm:w-1/2 md:w-5/12 lg:w-1/3 max-h-[90vh] rounded-lg shadow-lg m-4'}`}
-			overlayClassName={`fixed inset-0  flex items-center justify-center ${fullScreen ? 'z-50' : 'bg-black bg-opacity-50 backdrop-blur-sm z-50'}`}
+			className={`
+				popup-layout-opening ${isClosing ? 'popup-layout-closing' : ''}
+				relative m-auto
+				bg-c-lm-gray-100 dark:bg-c-dm-gray-900 border border-c-lm-gray-300 dark:border-c-dm-gray-800
+				transition-all duration-200
+				${fullScreen ? 'flex flex-col space-between w-full h-full' : 'p-6 w-full sm:w-1/2 md:w-5/12 lg:w-1/3 rounded-xl shadow-lg m-4'}
+				${className}
+			`}
+			overlayClassName={`
+				fixed inset-0 flex flex-col py-10 overflow-y-auto custom-scrollbar overflow-x-hidden
+				${fullScreen ? 'z-50' : 'bg-black bg-opacity-50 backdrop-blur-sm z-50'}
+				popup-overlay-opening ${isClosing ? 'popup-overlay-closing' : ''}
+			`}
 			bodyOpenClassName="overflow-hidden"
 		>
-
 			<div className={`${fullScreen && 'h-full'}`}>
 				{fullScreen && <Header toggleSidebar={() => { }} />}
+					
 				<div className={`${fullScreen && 'px-6 py-6 flex flex-col justify-between'}`}>
 					{children}
 				</div>
